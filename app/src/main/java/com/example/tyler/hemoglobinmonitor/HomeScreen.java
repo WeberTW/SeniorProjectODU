39cf64135260cd34eb5ba9ec7deab56e043457ef
WeberTW <tmwhittle16@gmail.com>
2017-04-03 19:43:05 -0400

    Application now has file I/O and saves all values received via bluetooth.
    Also, trend data is now being displayed from that as well as most recent value to home screen.

--- app/src/main/java/com/example/tyler/hemoglobinmonitor/HomeScreen.java
+++ app/src/main/java/com/example/tyler/hemoglobinmonitor/HomeScreen.java
@@ -4,16 +4,67 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class HomeScreen extends AppCompatActivity {
+
+    //Need these
+    TextView myLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        myLabel = (TextView)findViewById(R.id.label);
+
+        //Make sure the file we want exists
+        File file = new File(getApplicationContext().getFilesDir(), "Values.txt");
+        if(!file.exists()){
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        //Get the most recent hemoglobin value
+        //Need to get from output file
+        FileInputStream inStream = null;
+        try {
+            inStream = openFileInput("Values.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        int c;
+        String displayValue = "";
+        try {
+            c = inStream.read();
+            while(c != -1 && c != 32){
+                displayValue = displayValue + Character.toString((char) c);
+                c = inStream.read();
+            }
+            inStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //See if we got a valid result
+        if(displayValue != "") {
+            //Now display it
+            String displaying = displayValue + " g/DL";
+            myLabel.setText(displaying);
+        }
+    }
+
+    private void msg(String s) {
+        Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
     }
 
     /** Called when the user clicks the View Trend Information button */
